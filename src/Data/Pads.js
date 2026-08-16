e604ca423cb3f154d53aadb6bed7743e25de2449
walid-amjahdi <w.amjahdi3996@uca.ac.ma>
2026-08-16 13:25:34 +0100

    8:29 16/8

---
+++ src/Data/Pads.js
@@ -0,0 +1,32 @@
+export const Pads = [
+    {
+        id: 1,
+        color: "#F18D8B",
+        on: true
+    },
+    {
+        id: 2,
+        color: "#F5C280",
+        on: false
+    },
+    {
+        id: 3,
+        color: "#EEEC79",
+        on: true
+    },
+    {
+        id: 4,
+        color: "#64ED98",
+        on: false
+    },
+    {
+        id: 5,
+        color: "#63DEED",
+        on: false
+    },
+    {
+        id: 6,
+        color: "#8776E0",
+        on: false
+    }
+]
